47c5c394025b86efdac8fc7a380fde1e39232db7
qwertystig <stigmctwist@gmail.com>
2023-04-25 09:43:27 +0200

    Local Repositort created

---
+++ APIDEE/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using APIDEE.Models;
+namespace APIDEE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+       public  string GetProduct ()
+        {
+            return "Milk";
+        }
+        [HttpGet]
+        [Route("All")]
+        public IEnumerable<Product> GetProducts() 
+        {
+            return new List<Product> { new Product(1, "bread"),
+                                      new Product    (2, "milk"),
+               new Product (3, "meat"),
+               new Product (4, "oil"),
+               new Product (5, "sweets"),
+               new Product (6, "fruits"),
+               new Product (7, "chicken"),
+               new Product(8, "weed")  };
+        }
+    }
+}
---
+++ APIDEE/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace APIDEE.Models
+{
+    public class Product
+    {
+        public int code { get; set; }
+        public string name { get; set; }
+
+        public Product(int code, string name)
+        {
+            this.code = code;
+            this.name = name;   
+        }
+    }
+}
